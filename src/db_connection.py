34f5cc06d7c850cf453c25d01053862ec40a031c
Michael <michael.mjpr@gmail.com>
2026-07-06 06:47:12 -0300

    Update db_connection.py
    Conexão Olap (Data WareHouse)

--- src/db_connection.py
+++ src/db_connection.py
@@ -18,6 +18,19 @@ def get_oltp_engine():
     
     return engine
 
+def get_olap_engine():
+    """Cria e retorna a engine de conexão com o banco OLAP (Data Warehouse)."""
+    user = os.getenv("OLAP_USER").strip()
+    password = os.getenv("OLAP_PASSWORD").strip()
+    db = os.getenv("OLAP_DB").strip()
+    host = "localhost"
+    port = "5435" 
+    
+    connection_string = f"postgresql://{user}:{password}@{host}:{port}/{db}?client_encoding=utf8"
+    engine = create_engine(connection_string)
+    
+    return engine
+
 if __name__ == "__main__":
     print("Iniciando teste de conexão...")
     engine = get_oltp_engine()
